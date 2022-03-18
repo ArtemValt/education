e1a8d97b9c5cd426349a1d82819df424f7aea843
ArtemValt <artemvalt12@gmail.com>
2022-03-18 19:08:15 +0400

    ex 1 and 2 3

--- src/main/java/stage2/practice/one/Main1.java
+++ src/main/java/stage2/practice/one/Main1.java
@@ -7,8 +7,11 @@ public class Main1 {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите выражение");
-        String str = sc.nextLine();
-        Task1 test1 = new Task1();
-        test1.test(str);
+//        String str = sc.nextLine();
+//        Task1 test1 = new Task1();
+//        test1.test(str);
+        Task1Stack task1Stack = new Task1Stack();
+        task1Stack.Task1(sc.nextLine());
+
     }
 }
---
+++ src/main/java/stage2/practice/one/Task1Stack.java
@@ -0,0 +1,55 @@
+package stage2.practice.one;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public class Task1Stack {
+     static Stack<Character> stack = new Stack<>();
+     static final List<Character> OPEN_BRACKETS = Arrays.asList('(', '{', '[');
+     static final List<Character> CLOSING_BRACKETS = Arrays.asList(')', '}', ']');
+    public void Task1(String s) {
+        char[] textChar = s.toCharArray();
+        for (int i = 0; i < textChar.length; i++) {
+            if (OPEN_BRACKETS.contains(textChar[i]))
+                stack.push(textChar[i]);
+            else {
+                if (CLOSING_BRACKETS.contains(textChar[i])) {
+                    if (!stack.empty() && stack.peek() == getOpenBracketPair(textChar[i])) {
+                        stack.pop();
+                    } else {
+                        if (stack.empty()) {
+                            System.out.println("Скобки расставлены не правильно! Нет открывающей " + getOpenBracketPair(textChar[i]));
+                            return;
+                        }
+
+                        char symbol = ' ';
+                        while (!stack.empty()) {
+                            if (stack.peek() == getOpenBracketPair(textChar[i])) {
+                                System.out.println("Скобки расставлены не правильно! Нет закрывающей " + getClosingBracketPair(symbol));
+                                return;
+                            }
+                            symbol = stack.pop();
+                        }
+
+                        System.out.println("Скобки расставлены не правильно! Нет открывающей " + getOpenBracketPair(textChar[i]));
+                        return;
+                    }
+                }
+            }
+        }
+        if (!stack.empty()) {
+            System.out.println("Скобки расставлены не правильно! Нет закрывающей " + getClosingBracketPair(stack.pop()));
+            return;
+        }
+        System.out.println("Скобки расставлены правильно!");
+    }
+    public  char getOpenBracketPair(char bracket) {
+        return (bracket == '}') ? '{' : (bracket == ')') ? '(' :
+                (bracket == ']') ? '[' : '\0';
+    }
+    public  char getClosingBracketPair(char bracket) {
+        return (bracket == '{') ? '}' : (bracket == '(') ? ')' :
+                (bracket == '[') ? ']' : '\0';
+    }
+}
